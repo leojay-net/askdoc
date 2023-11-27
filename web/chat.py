ce4427f92457ce5b903b40b45ba8d06559ddbe52
leojay-net <aleonomohjoseph03@gmail.com>
2023-11-27 15:55:24 +0100

    update ai endpoint

--- web/chat.py
+++ web/chat.py
@@ -13,7 +13,7 @@ def get_response(problem, symptoms, medical_history):
     response = client.chat.completions.create(
     model="gpt-3.5-turbo-1106",
     messages=[
-        {"role": "system", "content": "You are a helpful medical assistant, You are diagnose and offer symptoms guidance and health tips, also emphasizing on how the the attention of a medical professional is needed regardless. "},
+        {"role": "system", "content": "You are a helpful medical assistant, You are diagnose and offer symptoms guidance and health tips, also emphasizing on how the the attention of a medical professional is needed regardless. You can also give out first aid things to do before seeking the attention of a medical professional "},
         {"role": "user", "content": "I am suffering from feeling really tired lately and I have been experiencing anemia and head aches. My medical history includes 'depression, unable to sleep'. What do I do?"},
         {"role": "assistant", "content": "I'm sorry to hear that you've been feeling tired and experiencing symptoms of anemia and headaches. Given your medical history of depression and difficulty sleeping, it's important to consult with a healthcare professional. Persistent fatigue can have various causes, including underlying medical conditions or inadequate sleep. Anemia may contribute to fatigue, and a thorough evaluation of your symptoms, medical history, and possibly further diagnostic tests will be essential.\
         It's crucial to address your mental health concerns as well. Depression and sleep disturbances can significantly impact your overall well-being. A healthcare provider can guide you through a comprehensive assessment and develop a tailored treatment plan, which may include lifestyle modifications, counseling, or medications.\
@@ -28,7 +28,7 @@ def get_response(problem, symptoms, medical_history):
     ],
     #response_format={"type": "json_object"},
     temperature=0.7,
-        max_tokens=300,
+        max_tokens=500,
         n=1,
         stop=None
     )
